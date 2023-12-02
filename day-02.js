99bf3e18f47f925abee599fd7e2052a6769af362
eleanor-rf <eleanorfrank@outlook.com>
2023-12-02 11:42:19 +0000

    day 2 puzzle 2 complete

--- day-02.js
+++ day-02.js
@@ -1,27 +1,31 @@
-function puzzle1(input){
+function findMaxValue(inputString, color) {
+    const regex = new RegExp(`(\\d+)\\s+${color}`, 'g');
+    let max = 0;
+    let match;
+
+    while ((match = regex.exec(inputString)) !== null) {
+        const value = parseInt(match[1]);
+        max = Math.max(max, value);
+    };
+    return max;
+};
+
+let gameList = [];
+function parseInput(input){
     inputSplit = input.split(/\r?\n/);
-    let gameList = [];
     for (const game of inputSplit){
         gameList.push(game.substring(game.indexOf(": ") + 2));
     };
+    return gameList;
+}
 
-    function findMaxValue(inputString, color) {
-        const regex = new RegExp(`(\\d+)\\s+${color}`, 'g');
-        let max = 0;
-        let match;
-
-        while ((match = regex.exec(inputString)) !== null) {
-            const value = parseInt(match[1]);
-            max = Math.max(max, value);
-        };
-        return max;
-    };
-
+function puzzle1(input){
+    parseInput(input);
     let invalidGames = [];
-    for (let i = 0; i < gameList.length; i ++){
+    for (let i = 0; i < gameList.length; i ++) {
         const maxRed = findMaxValue(gameList[i], 'red');
         const maxGreen = findMaxValue(gameList[i], 'green');
-        const maxBlue = findMaxValue(gameList[i], 'blue')
+        const maxBlue = findMaxValue(gameList[i], 'blue');
 
         if ((maxRed > 12) && invalidGames.indexOf((i + 1) === -1)) {
             invalidGames.push(i + 1);
@@ -42,3 +46,11 @@ function puzzle1(input){
     return 5050 - sum;
 };
 
+function puzzle2(input){
+    parseInput(input);
+    let sum  = 0;
+    for (const game of gameList) {
+        sum += findMaxValue(game, 'red') * findMaxValue(game, 'blue') * findMaxValue(game, 'green');
+    };
+    return sum;
+};
