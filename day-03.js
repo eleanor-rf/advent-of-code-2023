c5794be507c9ee8fff877578151d9883b2f5666e
eleanor-rf <eleanorfrank@outlook.com>
2023-12-03 18:39:25 +0000

    day 3 part 2 complete

--- day-03.js
+++ day-03.js
@@ -2,8 +2,8 @@ inputSplit = input.split(/\r?\n/);
 
 // regex test if a character is a special character
 // won't match 'undefined' - takes care of bug when number is at start/end of row
-function isSpecial(str){
- return /[!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]/g.test(str);
+function isSpecial(char){
+ return /[!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]/g.test(char);
 }
 
 function isTouchingASymbol(array,row,column,length) {
@@ -59,8 +59,10 @@ function findNumberStartAndLength(input) {
                     // found the end of the number, now push its position and length to result array
                     data.push({
                         row: row,
-                        column: startColumn,
+                        startColumn: startColumn,
                         length: length,
+                        endColumn: startColumn + length-1,
+                        number: input[row].substring(startColumn,startColumn+length)
                     });
                     startColumn = -1;
                     length = 0;
@@ -75,7 +77,7 @@ let numberList = findNumberStartAndLength(inputSplit);
 // check all of the found numbers and if they are touching a symbol, push to the engineParts array
 let engineParts = [];
 for (const data of numberList) {
-    if(isTouchingASymbol(inputSplit,data['row'],data['column'],data['length']) === true) {
+    if (isTouchingASymbol(inputSplit,data['row'],data['column'],data['length']) === true) {
         engineParts.push(inputSplit[data['row']].substring(data['column'],data['column'] + data['length']));
     }
 }
@@ -85,3 +87,79 @@ sum = 0;
 for (const part of engineParts){
     sum += parseInt(part);
 }
+
+// part 2 - gear ratios
+// find the positions of the *s
+function findStars(input){
+    let data = []
+    for (let row = 0; row < input.length; row++){
+        for (let column = 0; column < input[row].length; column++){
+            if (input[row][column] === '*'){
+                data.push({
+                    row: row,
+                    column: column
+                })
+            }
+        }
+    }
+    return data;
+}
+
+// iterate through each star and check overlap with numbers in the 8 surrounding squares
+function traverse(input) {
+    let stars = findStars(input);
+    let numbers = findNumberStartAndLength(input);
+    for (let i = 0; i < stars.length; i ++) {
+        // iterate through each star
+        let starRow = stars[i]['row'];
+        let starColumn = stars[i]['column'];
+        let starNeighbours = [];
+        for (let j = 0; j < numbers.length; j ++){
+            // for each star, check each number in numbers
+            // check for neighbours on the same row
+            if (
+                (numbers[j]['row'] === starRow) &&
+                ((numbers[j]['endColumn'] === starColumn - 1) || numbers[j]['startColumn'] === starColumn + 1)
+                ) {
+                starNeighbours.push(numbers[j]['number']);
+            }
+            // check for neighbours on the row above
+            if (
+                (numbers[j]['row'] === starRow - 1 &&
+                ((starColumn - 1 <= numbers[j]['startColumn'] && numbers[j]['startColumn'] <= starColumn + 1) ||
+                (starColumn - 1 <= numbers[j]['endColumn'] && numbers[j]['endColumn'] <= starColumn + 1))
+                )
+                ) {
+                starNeighbours.push(numbers[j]['number'])
+            }
+            // check for neighbours on the row below
+            if (
+                (
+                numbers[j]['row'] === starRow + 1 &&
+                ((starColumn - 1 <= numbers[j]['startColumn'] && numbers[j]['startColumn'] <= starColumn + 1) ||
+                (starColumn - 1 <= numbers[j]['endColumn'] && numbers[j]['endColumn'] <= starColumn + 1))
+                )
+                ) {
+                starNeighbours.push(numbers[j]['number'])
+            }
+            stars[i]['neighbours'] = starNeighbours;
+        }
+    }
+    return stars;
+}
+
+function calculateRatios(input) {
+    const starMap = traverse(inputSplit);
+    ratios = [];
+    for (let i = 0; i < starMap.length; i ++) {
+        if (starMap[i]['neighbours'].length === 2) {
+            ratios.push(parseInt(starMap[i]['neighbours'][0])*parseInt(starMap[i]['neighbours'][1]))
+        }
+    }
+    let sum = 0;
+    for (let j = 0; j < ratios.length; j++){
+        sum += ratios[j];
+    }
+    return sum;
+}
+
